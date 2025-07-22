53988d0af65d4ed029a7e3913c778b29b4ab880f
Jesster Gottic <mampacquing@tip.edu.ph>
2025-07-22 19:47:13 +0800

    angel

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 URL configuration for careconnect_backend project.
 
@@ -27,3 +28,20 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+=======
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PatientProfileViewSet, DonorViewSet, LoyaltyTierViewSet, AuctionItemViewSet, BidViewSet, DonationViewSet
+
+router = DefaultRouter()
+router.register(r'patients', PatientProfileViewSet)
+router.register(r'donors', DonorViewSet)
+router.register(r'loyalty-tiers', LoyaltyTierViewSet)
+router.register(r'auction-items', AuctionItemViewSet)
+router.register(r'bids', BidViewSet)
+router.register(r'donations', DonationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+] 
+>>>>>>> e12b7af (angel commit)
