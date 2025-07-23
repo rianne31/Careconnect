63e4229fa29035fc1b6113d64e4c2133e67e5aa0
RAMISSESS <raphsantos04@gmail.com>
2025-07-23 19:22:06 +0800

    Cleaned up root dir, removed dublication

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from .models import PatientProfile, Donor, LoyaltyTier, AuctionItem, Bid, Donation
-
-admin.site.register(PatientProfile)
-admin.site.register(Donor)
-admin.site.register(LoyaltyTier)
-admin.site.register(AuctionItem)
-admin.site.register(Bid)
-admin.site.register(Donation)
